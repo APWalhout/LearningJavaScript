af0c1a9a996690d81eacfcdc2fe5cafc321ca635
Alex Walhout <alexpwalhout@gmail.com>
2019-11-16 18:12:21 -0800

    challenge

--- Basics/strings.js
+++ Basics/strings.js
@@ -3,5 +3,12 @@ let nameMid = '';
 let nameLast = 'Walhout';
 let nameFull = nameFirst + ' ' + nameMid + ' ' + nameLast;
 
+//course challenge:
+let city = 'Seattle';
+let country = 'United States';
+let location = 'Location: ' + city + ', ' + country;
+
 console.log('Name: ' + nameLast + ', ' + nameFirst + nameMid);
 console.log('Full Name: ' + nameFull);
+//course challenge:
+console.log(location);
