048a8c0e6d6b32f31182639f20204da8e5947e0d
Alex Walhout <alexpwalhout@gmail.com>
2019-11-16 18:41:21 -0800

    clarified decisions

--- Basics/numbers.js
+++ Basics/numbers.js
@@ -16,6 +16,7 @@ console.log('My age in Dog Years: ' + dogYears);
 //challenge:
 let questionsTotal = 183;
 let questionsWrong = 23;
+//score and percentage could be combined, but this allows more flexibility. same with having questions wrong instead of those right
 let score = (questionsTotal - questionsWrong) / questionsTotal;
 let percentage = score * 100;
 
