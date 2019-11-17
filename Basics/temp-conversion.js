cd75c2ebedb24a4649332d9de7b7868eca20cb13
Alex Walhout <alexpwalhout@gmail.com>
2019-11-16 19:06:11 -0800

    Create temp-conversion.js

---
+++ Basics/temp-conversion.js
@@ -0,0 +1,10 @@
+let fahrenheit = 72;
+//calculate celsius
+let celsius = (fahrenheit - 32)/1.8;
+//calculate kelvin
+let kelvin = celsius + 273.15;
+
+console.log('Temperature in: ');
+console.log('Fahrenheit: ' + fahrenheit);
+console.log('Celsius: ' + celsius);
+console.log('Kelvin: ' + kelvin);
