0ff18418ad78546b883d0208805f941dccf396eb
Alex Walhout <alexpwalhout@gmail.com>
2019-11-20 12:46:59 -0800

    Update boolean-advanced.js
    updated formatting and completed challenge

--- Basics/boolean-advanced.js
+++ Basics/boolean-advanced.js
@@ -1,10 +1,24 @@
 let isAccountLocked = true;
+let userRole = 'admin';
 
 if (isAccountLocked)
 {
     console.log('Account is Locked');
+} else if (userRole === 'admin') {
+    console.log('Welcome Admin');
+} else {
+    console.log('Welcome');
 }
-else
+
+
+//challenge area:
+let temp = 45;
+
+if (temp <= 32)
 {
-    console.log('Account is Unlocked');
+    console.log('It is freezing outside!');
+} else if (temp > 80) {
+    console.log('It is too hot outside');
+} else {
+    console.log('Go outside, it is great');
 }
