fc2cb0586c102825f0a63e1527977b017e3aa39d
Alex Walhout <alexpwalhout@gmail.com>
2019-11-20 13:14:12 -0800

    Update scope.js

--- Basics/scope.js
+++ Basics/scope.js
@@ -2,6 +2,15 @@
 //{} is used to delimit scope
 //Global scope: defined outside all {} (wrapped in an invisible global {})
 //local scope: defined inside {}
+
+//in a scope you can access variables defined in that scope or in parent scopes
+//ie: local scopes can access global scope.
+
+//Global: var1
+    //local: var2
+        //local: var4
+    //local: var3
+
 let var1 = 'var1';//global
 
 if (true) 
@@ -9,4 +18,14 @@ if (true)
     console.log(var1);
     //causes error if used outside of if statement scope
     let var2 = 'var2';//local
+    
+    if (true)
+    {
+        let var4 = 'var4';//local
+    }
+}
+
+if (true)
+{
+    let var3 = 'var3';//local
 }
