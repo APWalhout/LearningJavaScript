1a554e95230aaf445e2f40a946f71af46533d8cb
Alex Walhout <alexpwalhout@gmail.com>
2019-11-20 13:01:51 -0800

    Update logical-and-or.js
    completed challenge and or

--- Basics/logical-and-or.js
+++ Basics/logical-and-or.js
@@ -1,7 +1,26 @@
 let temp = 65;
 
-//logical AND operator
+//logical AND operator: true iff A = true, B = true
 if ((temp >= 60) && (temp <= 85))
 {
     console.log('It is the perfect temperature outside');
+}
+
+//logical OR operator: true if at least one compared boolean is true
+if ((temp <= 0) || (temp >= 120))
+{
+    console.log('Do not go outside, you will die of exposure');
+}
+
+//challenge area:
+let isGuestOneVegan = true;
+let isGuestTwoVegan = false;
+
+if (isGuestOneVegan && isGuestTwoVegan) 
+{
+    console.log('Only Vegan Menus');
+} else if (isGuestTwoVegan || isGuestOneVegan) {
+    console.log('Both Options');
+} else {
+    console.log('Offer Any Options');
 }
