11563035db8278c43de293e02d2dc34ba10aea42
Alex Walhout <alexpwalhout@gmail.com>
2019-11-20 13:09:05 -0800

    Update scope.js
    scoping basics

--- Basics/scope.js
+++ Basics/scope.js
@@ -1,6 +1,12 @@
-let var1 = 'var1';
+//JS uses lexical aka static scoping
+//{} is used to delimit scope
+//Global scope: defined outside all {} (wrapped in an invisible global {})
+//local scope: defined inside {}
+let var1 = 'var1';//global
 
 if (true) 
 {
-    
+    console.log(var1);
+    //causes error if used outside of if statement scope
+    let var2 = 'var2';//local
 }
