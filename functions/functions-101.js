cd90f879ae7fea5a5e5fbc3709195794f9eddd67
Alex Walhout <alexpwalhout@gmail.com>
2019-11-20 19:54:20 -0800

    Update functions-101.js
    added return value and example

--- functions/functions-101.js
+++ functions/functions-101.js
@@ -4,3 +4,18 @@ let greetUser = function ()
 }
 
 greetUser();
+
+let square = function (num)
+{
+    //method 1
+    num *= num;
+    console.log(num);
+    //method 2
+    let result = num * num;//preserves data
+    return result;//actually passes the data along
+}
+
+let num = 3;
+square(num);
+console.log('Calling function result below: ');
+console.log(square(num));
