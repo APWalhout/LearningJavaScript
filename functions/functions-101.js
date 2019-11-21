e3bc6622a85bbbd9b00305957c83098a23088f9e
Alex Walhout <alexpwalhout@gmail.com>
2019-11-20 20:01:41 -0800

    challenge complete

--- functions/functions-101.js
+++ functions/functions-101.js
@@ -19,3 +19,15 @@ let num = 3;
 square(num);
 console.log('Calling function result below: ');
 console.log(square(num));
+
+//challenge area:
+let convertFahrenheitCelsius = function(fahrenheit)
+{
+    let celsius = (fahrenheit - 32) / 1.8;
+
+    return celsius;
+}
+
+console.log('Conversion of 45 degrees F: ' + convertFahrenheitCelsius(45));
+console.log('Conversion of 32 degrees F: ' + convertFahrenheitCelsius(32));
+console.log('Conversion of 120 degrees F: ' + convertFahrenheitCelsius(120));
