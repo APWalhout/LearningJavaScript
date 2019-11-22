713df3d9dcab94350aa3597645d01d6ac28559a6
Alex Walhout <alexpwalhout@gmail.com>
2019-11-22 09:55:57 -0800

    Update arguments.js
    learned how to default an argument

--- functions/arguments.js
+++ functions/arguments.js
@@ -5,3 +5,12 @@ let add = function (a, b)
 
 let result = add(10, 1);
 console.log(result);
+
+//default arguments are given as equivalency in the argument list
+let getScoreText = function (name = 'Player1', score = 0)
+{
+    console.log(name);
+    console.log(score);
+}
+
+getScoreText();
