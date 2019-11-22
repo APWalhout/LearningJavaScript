517f46efc6fbb9ba4c7d0ec48d2f2901fcbfa671
Alex Walhout <alexpwalhout@gmail.com>
2019-11-22 14:18:20 -0800

    Update string-methods.js
    JS documentation source

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -2,3 +2,8 @@ let name = 'Alex Walhout';
 
 //length property
 console.log(name.length);
+
+//convert to upper case
+console.log(name.toUpperCase());
+//documentation on mdn string in google
+//a developer.mozilla.org page
