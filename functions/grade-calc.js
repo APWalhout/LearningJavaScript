526342b546afea99dc1212e2f39a3f9787381674
Alex Walhout <alexpwalhout@gmail.com>
2019-11-22 11:12:38 -0800

    Update grade-calc.js
    completed section challenge function

--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -3,5 +3,27 @@
 let gradeCalc = function (score, total)
 {
     let percent = (score/total) * 100;
-    return `Grade is: ${}, obtaining a score of ${}% from ${score}/${total} correct answers.`;
+    let grade = 'NA';
+
+    if (percent >= 90)
+    {
+        grade = 'A';
+    } else if (percent >= 80) {
+        grade = 'B';
+    } else if (percent >= 70) {
+        grade = 'C';
+    } else if (percent >= 60){
+        grade = 'D';
+    } else {
+        grade = 'F';
+    }
+
+    return `Grade is: ${grade}, obtaining a score of ${percent}% from ${score}/${total} correct answers.`;
 }
+//tests
+console.log('Expect A:');
+console.log(gradeCalc(90, 100));
+console.log('Expect C:');
+console.log(gradeCalc(73.5, 100));
+console.log('Expect F:');
+console.log(gradeCalc(20, 675));
