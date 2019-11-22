786dc728d37d76d96b2bd9b7ea98fae7f848c3f7
Alex Walhout <alexpwalhout@gmail.com>
2019-11-22 13:40:35 -0800

    Update objects-references.js
    completed challenge lesson

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -19,3 +19,33 @@ addExpense(myAccount, 2.50);
 console.log(myAccount);
 //myAccount is printed twice and in an altered form
 //account is a 'pointer' alias to myAccount so when messing with account, it changes the object data it represents
+
+//challenge area:
+let addIncome = function (account, amount)
+{
+    account.income += amount;
+}
+
+let resetAccount = function (account)
+{
+    account.expenses = 0;
+    account.income = 0;
+}
+
+let getAccountSummary = function (account)
+{
+    return `Account for ${account.name} has a balance of $${account.income - account.expenses} from $${account.expenses} for expenses and an income of $${account.income}.`;
+}
+
+//add income
+addIncome(myAccount, 100);
+//add expense
+addExpense(myAccount, 20);
+//add expense
+addExpense(myAccount, 32);
+//get summary
+console.log(getAccountSummary(myAccount));
+//reset account
+resetAccount(myAccount);
+//get summary
+console.log(getAccountSummary(myAccount));
