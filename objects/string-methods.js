a73e7a611d2b42a431d6a5425ef9f732be4e0a8f
Alex Walhout <alexpwalhout@gmail.com>
2019-11-22 14:30:26 -0800

    Update string-methods.js
    completed the challenge

--- objects/string-methods.js
+++ objects/string-methods.js
@@ -7,3 +7,29 @@ console.log(name.length);
 console.log(name.toUpperCase());
 //documentation on mdn string in google
 //a developer.mozilla.org page
+
+console.log(name.toLowerCase());
+
+//includes method
+let password = 'abc123password098';
+console.log(password.includes('password'));
+
+//trim() removes extra white space before and after a string without touching middle
+let trimStr = '  Alex Wal  hout ';
+console.log(trimStr.trim());
+
+//challenge area:
+let isValidPassword = function (passcode)
+{
+    let wordCheck = passcode.toLowerCase();//this step allows quick checks for all forms of 'password' occurrence
+    if ((passcode.length < 8) || (wordCheck.includes('password')))
+    {
+        return false;
+    }
+    return true;
+}
+
+console.log(isValidPassword('PaSSwoRd'));
+console.log(isValidPassword('password'));
+console.log(isValidPassword('123'));
+console.log(isValidPassword('123abcd985frt'));
