5129b124963dce7c182886df63c97e2c5567ea4e
Alex Walhout <alexpwalhout@gmail.com>
2019-11-22 15:27:44 -0800

    added to array knowledge and completed challenge

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,3 +1,5 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];//note that array items can be of mixed types in JS
 
 console.log(notes.length);//remember documentation on mdn array
+console.log(notes[0]);//indices start at 0 as they should!
+//note that accessing out of bounds indices doesn't crash or error, it simply returns undefined
---
+++ arrays/todo.js
@@ -0,0 +1,8 @@
+//create an array of 5 things to do
+//print message includes length
+//print first and second to last items to console
+const todo = ['Take dog to vet','1 mile run','Pick up beans','Put cat in bag','Groceries'];
+
+console.log(`You have ${todo.length} things to do:`);
+console.log(`First --> ${todo[0]}`);
+console.log(`Penultimate --> ${todo[todo.length - 2]}`);
