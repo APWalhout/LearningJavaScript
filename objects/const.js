b031e0111221df092aefd29472e3bc52d954e539
Alex Walhout <alexpwalhout@gmail.com>
2019-11-22 14:55:53 -0800

    Update const.js
    learned how const works to protect data

--- objects/const.js
+++ objects/const.js
@@ -3,3 +3,15 @@ let isRaining = true;
 isRaining = false;
 
 console.log(isRaining);
+
+const canRain = true;//makes a varaible in the same way let does, but it protects data integrity
+
+//canRain = false; //can't do this, cant reassign a const
+
+console.log(canRain);
+
+const person = {
+    age: 23
+}
+
+//person = {};//this fails as can't reassign, but can still access properties and give properties new values
