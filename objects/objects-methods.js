3d2d043857b9577450a83c9a319804cd13a7c4c4
Alex Walhout <alexpwalhout@gmail.com>
2019-11-22 13:54:42 -0800

    Update objects-methods.js

--- objects/objects-methods.js
+++ objects/objects-methods.js
@@ -1,12 +1,27 @@
 let restaurant = {
     name: 'cosi',
     guestCapacity: 75,
-    guestCount: 0,
+    guestCount: 73,
     //method is a function that is a property of an object, a helper method
     checkAvailability: function (partySize) 
     {
-        console.log(partySize);
-    } 
+        let seatsLeft = this.guestCapacity - this.guestCount;
+        return partySize <= seatsLeft;
+    },
+    seatParty: function (partySize)
+    {
+        //could throw in a reference to checkavailability as an error catch
+        this.guestCount += partySize;
+    },
+    removeParty: function (partySize)
+    {
+        this.guestCount -= partySize;
+    }
 }
 
-restaurant.checkAvailability(4);
+//seatParty
+//removeParty
+
+let status = restaurant.checkAvailability(4);
+restaurant.removeParty(4)
+console.log(status);
