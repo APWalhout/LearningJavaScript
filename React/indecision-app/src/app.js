f9c785176e5e4297dca8f64973bf4b78716bdddb
Alex Walhout <alexpwalhout@gmail.com>
2019-11-25 16:27:41 -0800

    set up a babel watch for live compilation to a clone app.js file in src and scripts

--- React/indecision-app/src/app.js
+++ React/indecision-app/src/app.js
@@ -1,12 +1,10 @@
-'use strict';
-
 //src app.js will have the JSX, the scripts app.js contains the babel conversions.
 console.log('App.js is running');
 
 //JSX - JavaScript XML
 //template is static, no variable injection
 //let template = <p>This is JSX from app.js</p>;//needs to be compiled in BABEL to compatable code below in babeljs.io using env and react plugins
-var template = React.createElement('h1', null, 'Indecision App');
-var appRoot = document.getElementById('app'); //retrieves a <div> to render to
+let template = React.createElement('h1', null, 'Indecision App');
+let appRoot = document.getElementById('app');//retrieves a <div> to render to
 
 ReactDOM.render(template, appRoot);
