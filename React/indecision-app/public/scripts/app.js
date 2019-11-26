32fddc775a98337501a3dbac25a57ad067f19c43
Alex Walhout <alexpwalhout@gmail.com>
2019-11-25 16:10:46 -0800

    installed babel plugins env and react
    react 6.24.1 and env 1.5.2

--- React/indecision-app/public/scripts/app.js
+++ React/indecision-app/public/scripts/app.js
@@ -2,7 +2,7 @@ console.log('App.js is running');
 
 //JSX - JavaScript XML
 //template is static, no variable injection
-//let template = <p>This is JSX from app.js</p>;//needs to be compiled in BABEL to compatable code below in babeljs.io
+//let template = <p>This is JSX from app.js</p>;//needs to be compiled in BABEL to compatable code below in babeljs.io using env and react plugins
 let template = React.createElement('p', null, 'This is JSX from app.js!');
 let appRoot = document.getElementById('app');//retrieves a <div> to render to
 
