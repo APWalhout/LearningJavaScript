c8cf33c0292e091438849991c1abe8650950adc6
Alex Walhout <alexpwalhout@gmail.com>
2020-02-04 10:55:45 -0800

    got to stopping point

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,4 @@
-const notes = [{}, {
+const notes = [{
     title: 'My next trip',
     body: 'Germany'
 }, {
@@ -9,41 +9,25 @@ const notes = [{}, {
     body: 'Get new chair'
 }] 
 
-//notes.push('My new note');
+const findNote = function(notes, noteTitle) {
+    const index = notes.findIndex(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase();
+    });
+    return notes[index];
+}
+
+const note = findNote(notes, 'office todo');
+console.log(note);
 
 console.log(notes.length);//remember documentation on mdn array
 console.log(notes);//indices start at 0 as they should!
-//note that accessing out of bounds indices doesn't crash or error, it simply returns undefined
 
-//notes.pop();//produces the popped object as a return
 
-//console.log(notes);
+// console.log(notes);
 
-//notes.shift();//pops at front
+// const index = notes.findIndex(function(note, index){
+//     console.log(note);
+//     return note.title === 'Goals'
+// });
 
-//console.log(notes);
-
-//notes.unshift('My first note');//pushes front
-
-//console.log(notes);
-
-//notes.splice(1,1);//first argument is which item to start at, second argument is the number of things wished to be removed
-//the third argument adds an item at the index and shoves everything after it down one index
-//notes.splice(1,0, 'my mid note');//this line doesn't remove an item, just adds. could remove if wanted
-//you can replace an item with splice, but it is easier at times to access an index and reassign it
-//notes[1] = 'my second mid note';
-
-//forEach runs a number of times = to the number of elements
-//notes.forEach(function (item, index) {
-//    console.log(item);
-//    console.log(index);
-//});
-
-console.log(notes);
-
-const index = notes.findIndex(function(note, index){
-    console.log(note);
-    return note.title === 'Goals'
-});
-
-console.log(index);
+// console.log(index);
