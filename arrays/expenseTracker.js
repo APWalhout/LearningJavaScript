857d905abc6edabbbab7f2bc5edff9a244779db7
Alex Walhout <alexpwalhout@gmail.com>
2020-02-19 16:31:44 -0800

    Update expenseTracker.js
    tidied some methods and reference values

--- arrays/expenseTracker.js
+++ arrays/expenseTracker.js
@@ -3,17 +3,17 @@ const account = {
     expenses: [],
     expenseTotal: 0,
     addExpense: function (description, amount) {
-        account.expenses.splice(0,0, {description: description, amount: amount });
-        account.expenseTotal += amount;//update total as you go to avoid constant recounting for each summary call
+        this.expenses.push({description: description, amount: amount });
+        this.expenseTotal += amount;//update total as you go to avoid constant recounting for each summary call
     },
     //getAccountSummary totals up all expenses and prints an account message summary 'account name has X in expenses'
     getAccountSummary: function() {
-    return account.expenseTotal;
+    return this.expenseTotal;
     },
     //does a live count of expense total as a back up in case of an error maybe from erased expenses, but also to learn for each loops
     refreshSummary: function() {
         let total = 0;
-        account.expenses.forEach(element => total += element.amount);
+        this.expenses.forEach(element => total += element.amount);
         return total;
     }
 };
