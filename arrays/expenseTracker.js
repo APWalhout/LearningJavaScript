f39420f2bd927a99b4406e3037d45314e33dfdf7
Alex Walhout <alexpwalhout@gmail.com>
2020-02-19 16:20:40 -0800

    Update expenseTracker.js
    added helper methods and an ass covering method

--- arrays/expenseTracker.js
+++ arrays/expenseTracker.js
@@ -1,6 +1,7 @@
 const account = {
     name: 'Alex Walhout',
-    expenses: []
+    expenses: [],
+    expenseTotal: 0
 };
 
 //expense object: props description: and amount
@@ -9,10 +10,25 @@ const account = {
 
 let addExpense = function(description, amount){
     account.expenses.splice(0,0, {description: description, amount: amount });
-}
+    account.expenseTotal += amount;//update total as you go to avoid constant recounting for each summary call
+};
 
 //getAccountSummary totals up all expenses and prints an account message summary 'account name has X in expenses'
+let getAccountSummary = function(){
+    return expenseTotal;
+};
+
+//does a live count of expense total as a back up in case of an error maybe from erased expenses, but also to learn for each loops
+let refreshSummary = function(){
+    let total = 0;
+    account.expenses.forEach(element => total += element.amount);
+    return total;
+};
+
+
 
 account.addExpense('Rent', 900);
 account.addExpense('Coffee', 6);
+console.log(account.getAccountSummary());
+account.expenseTotal = refreshSummary();
 console.log(account.getAccountSummary());
