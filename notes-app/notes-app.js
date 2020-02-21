13e26d760cd619c40bfd3c830a9b31510b4c8804
Alex Walhout <alexpwalhout@gmail.com>
2020-02-21 11:37:47 -0800

    learned query and removal

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,13 @@
-console.log('This is loaded in from a js file');
+//DOM - Document Object Model
+
+// const pTag = document.querySelector('p');//returns the first insance of the p tag.
+// pTag.remove();//removes all selected elements
+
+//querry all and remove
+const pTags = document.querySelectorAll('p');//returns ALL instances
+
+pTags.forEach(function(p){
+    p.textContent = '***';
+    // console.log(p.textContent);
+    //p.remove();
+});
